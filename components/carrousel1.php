9eefb28a278ac429a559be86a5516c438a15cdaa
Matias Otero <otero.matias.1987@gmail.com>
2024-11-23 20:55:13 -0300

    carrusel con 15 diapositivas en generacion.php

---
+++ components/carrousel1.php
@@ -0,0 +1,74 @@
+<div id="carouselExampleIndicators" class="carousel slide mt-5" data-bs-ride="carousel">
+<div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3" aria-label="Slide 4"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="4" aria-label="Slide 5"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="5" aria-label="Slide 6"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="6" aria-label="Slide 7"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="7" aria-label="Slide 8"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="8" aria-label="Slide 9"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="9" aria-label="Slide 10"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="10" aria-label="Slide 11"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="11" aria-label="Slide 12"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="12" aria-label="Slide 13"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="13" aria-label="Slide 14"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="14" aria-label="Slide 15"></button>
+</div>
+
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+            <img src="../assets/Diapositiva1.jpg" class="d-block w-100" alt="Imagen 1">
+        </div>
+        <div class="carousel-item">
+            <img src="../assets/Diapositiva2.jpg" class="d-block w-100" alt="Imagen 2">
+        </div>
+        <div class="carousel-item">
+            <img src="../assets/Diapositiva3.jpg" class="d-block w-100" alt="Imagen 3">
+        </div>
+        <div class="carousel-item">
+            <img src="../assets/Diapositiva4.jpg" class="d-block w-100" alt="Imagen 4">
+        </div>
+        <div class="carousel-item">
+            <img src="../assets/Diapositiva5.jpg" class="d-block w-100" alt="Imagen 5">
+        </div>
+        <div class="carousel-item">
+            <img src="../assets/Diapositiva6.jpg" class="d-block w-100" alt="Imagen 6">
+        </div>
+        <div class="carousel-item">
+            <img src="../assets/Diapositiva7.jpg" class="d-block w-100" alt="Imagen 7">
+        </div>
+        <div class="carousel-item">
+            <img src="../assets/Diapositiva8.jpg" class="d-block w-100" alt="Imagen 8">
+        </div>
+        <div class="carousel-item">
+            <img src="../assets/Diapositiva9.jpg" class="d-block w-100" alt="Imagen 9">
+        </div>
+        <div class="carousel-item">
+            <img src="../assets/Diapositiva10.jpg" class="d-block w-100" alt="Imagen 10">
+        </div>
+        <div class="carousel-item">
+            <img src="../assets/Diapositiva11.jpg" class="d-block w-100" alt="Imagen 11">
+        </div>
+        <div class="carousel-item">
+            <img src="../assets/Diapositiva12.jpg" class="d-block w-100" alt="Imagen 12">
+        </div>
+        <div class="carousel-item">
+            <img src="../assets/Diapositiva13.jpg" class="d-block w-100" alt="Imagen 13">
+        </div>
+        <div class="carousel-item">
+            <img src="../assets/Diapositiva14.jpg" class="d-block w-100" alt="Imagen 14">
+        </div>
+        <div class="carousel-item">
+            <img src="../assets/Diapositiva15.jpg" class="d-block w-100" alt="Imagen 15">
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+    </button>
+</div>
--- pages/generacion.php
+++ pages/generacion.php
@@ -6,21 +6,31 @@
     <title>Generacion</title>
     <link rel="stylesheet" href="../css/bootstrap.min.css">
     <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/styleGeneracion.css">
 </head>
 <body>
     <div class="container bg-primary-subtle">
+        <!-- Menú de navegación -->
         <?php include '../drivers/menuPages.php'; ?>
 
+        <!-- Título principal -->
         <h1 class="text-center text-primary">Generacion</h1>
 
-        <div class="row">
-            <div class="col"><?php include("../drivers/tabla_generacion.php"); ?></div>
-            <div class="col text-center">
-                <img src="../assets/mapa_camesa.png" alt="">
+        <!-- Texto adicional -->
+        <div class="row mt-5">
+            <div class="col-12">
+                <p class="texto-destacado text-center contenedor-ajustado">
+                    El presente documento ha sido elaborado con la mejor información disponible e interpretable a la fecha, la cual ha sido provista por Agentes del Mercado Eléctrico Mayorista (“Agentes”) y terceros. 
+                    SINERGIA asume que todos los datos suministrados por los Agentes son exactos, veraces, y correctos; producto de un adecuado y oportuno cumplimiento de las obligaciones que le competen como actores del Mercado Eléctrico Mayorista, así como de la normativa que les resulte aplicable. 
+                    SINERGIA no se responsabiliza por las consecuencias que puedan producirse como resultado del incumplimiento de las obligaciones que le competen a los Agentes ni del suministro de informaciones incorrectas, imprecisas e incompletas por parte de los mismos.
+                </p>
             </div>
         </div>
+        <!-- Carrusel de imágenes -->
+        <?php include '../components/carrousel1.php'; ?>
 
-    </div>
+
     <script src="../js/bootstrap.bundle.min.js"></script>
+    </div>
 </body>
 </html>
